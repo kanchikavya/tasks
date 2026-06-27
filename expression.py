ed95de32437fbceb818e0c32ef3c598740d80d66
kavya <kanchikavya2004@gmail.com>
2026-06-27 10:37:12 +0530

    expression.py

---
+++ expression.py
@@ -0,0 +1,33 @@
+# Arithmetic Expressions
+a=10
+b=5
+print(a+b)   
+print(a-b)    
+print(a*b) 
+print(a/b)   
+print(a%b)   
+print(a**b)  
+#area of rectangle
+length=10
+width=5
+area=length * width
+print("Area =",area)
+# simple interest
+p=1000
+r=5
+t=2
+si=(p*r*t)/100
+print("Simple Interest =",si)
+# relational expressions
+a=10
+b=20
+print(a>b)
+print(a<b)
+print(a==b)
+print(a!=b)
+# logical expressions
+a=10
+b=20
+print(a>5 and b<30)
+print(a>15 or b<30)
+print(not(a>b))
