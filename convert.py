a2375ba51800335b9fd43cd5c4d67749fe2a275f
kavya <kanchikavya2004@gmail.com>
2026-06-27 12:24:58 +0530

    convert.py

---
+++ convert.py
@@ -0,0 +1,27 @@
+a=10
+print(float(a))      
+print(str(a))        
+print(bool(a))      
+print(complex(a))    
+a=10.5
+print(int(a))       
+print(str(a))    
+print(bool(a))       
+print(complex(a))    
+a="123"
+print(int(a))        
+print(float(a))     
+print(bool(a))       
+print(complex(a))    
+a=True
+print(int(a))        
+print(float(a))      
+print(str(a))     
+print(complex(a))    
+a = 10 + 5j
+print(str(a))        
+print(bool(a))      
+a = 10 + 5j
+
+print(int(a))      
+print(float(a))   
